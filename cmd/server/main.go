64300f4bd8b18e5ca6b5b6689c1a3a8c0cb70eee
Thiago Kufa <tkufa.code@gmail.com>
2023-11-30 20:22:41 -0300

    :sparkles: feat: create products entities

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"time"
-
+	"github.com/ThiagoKufa/ref_api_go/internal/entities"
 	"github.com/ThiagoKufa/ref_api_go/internal/infra/database"
-	"gorm.io/gorm"
 )
-
-type User struct {
-	gorm.Model        // Inclui campos padrões do GORM como ID, CreatedAt, UpdatedAt, DeletedAt
-	Username   string `gorm:"unique;not null"`
-	Email      string `gorm:"unique;not null"`
-	Password   string `gorm:"not null"`
-	FirstName  string
-	LastName   string
-	BirthDate  time.Time
-	IsActive   bool `gorm:"default:true"`
-}
 
 func main() {
 	database.ConectDatabase()
-	database.Migrate(&User{})
+	database.Migrate(&entities.Product{})
 	database.CloseDatabaseConnection()
-
 }
---
+++ internal/entities/products.go
@@ -0,0 +1,19 @@
+package entities
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Product representa um produto no banco de dados
+type Product struct {
+	gorm.Model
+	Name        string     `gorm:"type:varchar(100);not null" json:"name"`
+	Description string     `gorm:"type:text;not null" json:"description"`
+	Price       float64    `gorm:"not null" json:"price"`
+	Quantity    int        `gorm:"not null" json:"quantity"`
+	IsActive    bool       `gorm:"default:true" json:"is_active"`
+	CreatedBy   string     `gorm:"type:varchar(100);not null" json:"created_by"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+}
